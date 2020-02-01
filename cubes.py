7c3878a5496fdbc83dac301b291212720ec1d5fd
Khoa <xuankhoa.nguyen@shopee.com>
2020-02-01 15:46:47 +0700

    Add

--- cubes.py
+++ cubes.py
@@ -6,7 +6,7 @@ def cubes(a):
     if 0 in x or 0 in y or 0 in z:
         return -1
 
-    t = x[0] * x[1] * x[2]  + y[0] * y[1] * y[2] + z[0] * z[1] * z[2]
+    t = x[0] * x[1] * x[2] + y[0] * y[1] * y[2] + z[0] * z[1] * z[2]
 
     m = round(t ** (1/3))
 
